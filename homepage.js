352785e93eb114764a8f80b0c848350827daf602
Shekhar Ranjan <shekhar10feb@gmail.com>
2022-09-04 15:42:27 +0530

    Did several changes

--- homepage.js
+++ homepage.js
@@ -2,7 +2,7 @@
 const str = localStorage.getItem('UsersData');
 
 // To convert string to an object 
-const parseObj = JSON.parse(str) || "[]";
+const parseObj = JSON.parse(str);
 
 document.querySelector('#fname').innerHTML = parseObj[0].fname;
 
