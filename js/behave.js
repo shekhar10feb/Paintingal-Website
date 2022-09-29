9d4c5b730ec6de17f1565b8e7c30edda8a0c9009
Shekhar Ranjan <shekhar10feb@gmail.com>
2022-09-29 14:27:44 +0530

    added codes

--- js/behave.js
+++ js/behave.js
@@ -1,40 +1,36 @@
-function signup(e){
-   // event.preventDefault();
-
-   let email = document.querySelector('#email').value;
-   let username = document.querySelector('#username').value;
-   let pwd = document.querySelector('#password').value;
+function signup(){
+   let fullname = document.querySelector('#fullname');
+   let email = document.querySelector('#email');
+   let username = document.querySelector('#username');
+   let pwd = document.querySelector('#password');
 
    let userArr = [];
 
    let user = {
-       email: email,
-       username: username,
-       password: pwd,
+      fullname: fullname.value,
+      email: email.value,
+      username: username.value,
+      password: pwd.value,
    }
 
    userArr.push(user);
 
    let json = JSON.stringify(userArr);
-   localStorage.setItem('user', json);
+   localStorage.setItem('UsersData', json);
    console.log('user added');
 }
 
 function loginFunc(e){
-   // event.preventDefault();
-
-   // console.log(123);
    let username = document.querySelector('#usernameLogin').value;
    let pwd = document.querySelector('#passwordLogin').value;
-   let result = document.querySelector('#result');
    let form = document.querySelector('.login');
 
-   let str = localStorage.getItem('user');
+   let str = localStorage.getItem('UsersData');
    let data = JSON.parse(str);
    console.log(username + ' ' + pwd);
    console.log(data);
 
-   if(str == null){
+   if(str == null || str == undefined || str == ''){
        alert('Wrong username!');
    } else if(username === data[0].username && pwd === data[0].password) {
        form.setAttribute('action', './homepage.html');
@@ -43,23 +39,25 @@ function loginFunc(e){
    }
 }
 
-const signupOption = document.querySelector('.signupOption');
 const signup1 = document.querySelector('.signup');
-const loginOption = document.querySelector('.loginOption');
 const login = document.querySelector('.login');
 const msg   = document.querySelector('.introMsg');
+const signupOption = document.querySelector('.signupOption');
+const loginOption = document.querySelector('.loginOption');
 
+// For clicking on Login form 
 loginOption.addEventListener('click', () => {
    login.classList.add('loginForm');
    signup1.classList.remove('signupForm');
    msg.innerHTML = `<p>Login here, upload your painting and show it to the world!</p>`;
-   console.log("Clicked!");
+    console.log("Clicked!");
 });
 
+// For clicking on Signup form 
 signupOption.addEventListener('click', () => {
    signup1.classList.add('signupForm');
    login.classList.remove('loginForm');
    msg.innerHTML = `<p>Register here, upload your painting and show it to the world!</p>`;
-})
+});
 
 
--- js/homepage.js
+++ js/homepage.js
@@ -1,15 +1,120 @@
 const logout = document.querySelector('.logout');
+
 // To get the string from localStorage
-const str = localStorage.getItem('UsersData');
+let str = localStorage.getItem('UsersData');
 
 // To convert string to an object 
 const parseObj = JSON.parse(str);
+console.log(parseObj);
 
-document.querySelector('#fname').innerHTML = parseObj[0].fname;
+document.querySelector('.name').innerHTML = parseObj[0].fullname;
+document.querySelector('.username').innerHTML = parseObj[0].username;
 
 logout.addEventListener('click', preventBack());
 
-function preventBack(){window.history.forward();}
-    setTimeout("preventBack()", 0);
-    window.onunload=function(){null};
+// To stop previous button of the browser
+function preventBack(){
+    window.history.forward();
+}
+    
+setTimeout("preventBack()", 0);
+window.onunload=function(){null};
+
+// To change the display picture 
+const profilePictureDiv = document.querySelector('.profilePicDiv');
+const profilePic = document.querySelector('#profilePic');
+
+const inputFile = document.getElementById('inputFile');
+const previewImage = profilePictureDiv.querySelector('.image-preview__image');
+
+profilePictureDiv.addEventListener('click', ()=>{
+  inputFile.click();
+});
+
+inputFile.addEventListener('change', getPicture);
+
+// This function will 
+function getPicture() {
+  const file = this.files[0];
+  
+  console.log(file);
+  console.log(`File name: ${file.name}`);
+  console.log(`Last modified: ${file.lastModified}`);
+  
+  if(file) {
+    const reader = new FileReader();
+    
+    previewImage.style.display = 'block';
+    
+    reader.addEventListener('load', function() {
+      // console.log(this);
+      previewImage.setAttribute('src', this.result);
+    });
+    
+    reader.readAsDataURL(file);
+  } else {
+    previewImage.style.display = null;
+    previewImage.setAttribute('src', '');
+  }
+}
+
+// Edit Button 
+const editBtn = document.querySelector('#editBtn');
+const usernameDiv = document.querySelector('.usernameDiv'); 
+
+editBtn.addEventListener('click', editBio);
+
+function editBio() {
+    const page = document.createElement('div');
+    page.classList.add('editBio');
+    
+    page.innerHTML = `
+                    <div class='closeBtn'>
+                      <p>Add your bio</p>
+                      <button id='close'>X</button>
+                    </div>
+                    <div class='inputDiv'>
+                      <textarea name="" id="inputBio" placeholder='Enter your bio'></textarea>
+                    </div>
+                    <div class='saveDiv'>
+                      <button type='button' id='saveBtn'>Save</button>                     
+                    </div>`;
+    usernameDiv.appendChild(page);
+
+    // To close the edit bio page 
+    const closeBtn = document.querySelector('#close');
+
+    closeBtn.onclick = function closeFunc() {
+        this.parentElement.parentElement.remove();
+
+        // We return 'false' to prevent the default action.
+        return false;
+    };
+
+    // To save using save button 
+    const saveBtn = document.querySelector('#saveBtn');
+    const bio = document.querySelector('#inputBio');
+
+    saveBtn.onclick = function saveFunc() {
+
+        document.querySelector('.bio').innerHTML = bio.value;
+        this.parentElement.parentElement.remove()
+        console.log('Hey!');
+        
+        // If no str data exist, then create an array 
+        // Otherwise, convert the localStorage string to an array 
+        str = str ? JSON.parse(str) : {};
+        
+        // Add new data to localStorage array 
+        str[0]['bio'] = bio.value;
+        
+        // Save back to localStorage 
+        localStorage.setItem('UsersData', JSON.stringify(str));
+        // page.style.display = 'none';
+        document.querySelector('.bio').innerHTML = str[0].bio;
+
+       // We return 'false' to prevent the default action. 
+       return false;
+    }
+};
 
